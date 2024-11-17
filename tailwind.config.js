c203f872fdc5b43e8213f0b66dc5aa0bf8b5147e
Yosmar Hinestroza <yosmarpc@gmail.com>
2024-11-17 19:22:24 -0300

    ajuste cap2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/preline/preline.js'
+
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin')
+  ],
 }
 
